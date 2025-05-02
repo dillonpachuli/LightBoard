b144611fc0aba524279b07aaf971b20f9b740b69
Dillon Chapuli <148895731+dillonpachuli@users.noreply.github.com>
2025-05-02 18:03:25 +0000

    first

---
+++ LightBoard.java
@@ -0,0 +1,49 @@
+public class LightBoard{
+    private boolean[][] lights;
+
+    public LightBoard(int numRows, int numCols){
+        lights = new boolean[numRows][numCols];
+        for (int i = 0; i < numRows; i++){
+            for (int j = 0; j < numCols; j++){
+                if (Math.random() < .4) lights[i][j] = true;
+                lights[i][j] = false;
+            }
+        }
+    }
+
+    public LightBoard(boolean[][] allLights){
+        lights = allLights;
+    }
+
+    public boolean evaluateLight(int row, int col){
+        if (lights[row][col]){
+            int lightsOn = 0;
+            for (int i = 0; i < lights.length; i++){
+                if (lights[i][col]) lightsOn++;
+            }
+            if (lightsOn % 2 == 0) return false;
+        }
+
+        if (!lights[row][col]){
+            int lightsOn = 0;
+            for (int i = 0; i < lights.length; i++){
+                if (lights[i][col]) lightsOn++;
+            }
+            if (lightsOn % 3 == 0) return true;
+        }
+
+        return lights[row][col];
+    }
+
+    public String toString(){
+        String s = "";
+        for (int i = 0; i < lights.length; i++){
+            if (i == 0) s += "[";
+            for (int j = 0; j < lights[i].length; j++){
+                if (j == lights[i].length-1) s += lights[i][j] + "]";
+                s += lights[i][j] + ", ";
+            }
+        }
+        return s;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,1 +1,20 @@
-
+public class Main{
+    public static void main(String[] args){
+        LightBoard L1 = new LightBoard(7, 5);
+        System.out.println(L1.toString());
+        boolean[][] lights = {
+            {true, true, false, true, true},
+            {true, false, false, true, false},
+            {true, false, false, true, true},
+            {true, false, false, false, true},
+            {true, false, false, false, true},
+            {true, true, false, true, true},
+            {false, false, false, false, false}
+        };
+        LightBoard L2 = new LightBoard(lights);
+        System.out.println(L2.evaluateLight(0, 3));
+        System.out.println(L2.evaluateLight(6, 0));
+        System.out.println(L2.evaluateLight(4, 1));
+        System.out.println(L2.evaluateLight(5, 4));
+    }
+}
